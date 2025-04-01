d1f3e5bea57a104cc5170ef408b8d02f4766946d
Alexdamastar <alexlumala33@gmail.com>
2025-03-31 20:46:24 -0700

    Initial Phone Book project, needs some bug testing

---
+++ C++ Projects/YourVeryOwnDict/BinarySearchTree.hpp
@@ -0,0 +1,53 @@
+#ifndef BINARYSEARCHTREE_HPP
+#define BINARYSEARCHTREE_HPP
+
+#include <iostream>
+
+using namespace std;
+
+template <typename K, typename V>
+class BinarySearchTree {
+private:
+    struct Node {
+        K key;
+        V value;
+        Node* left;
+        Node* right;
+        Node(K k, V v) : key(k), value(v), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+
+    void insert(Node*& node, K key, V value) {
+        if (!node) {
+            node = new Node(key, value);
+            return;
+        }
+        if (key < node->key) {
+            insert(node->left, key, value);
+        } else if (key > node->key) {
+            insert(node->right, key, value);
+        } else {
+            node->value = value;
+        }
+    }
+
+    V& search(Node* node, K key) {
+        if (!node) throw runtime_error("Key not found");
+        if (key == node->key) return node->value;
+        return (key < node->key) ? search(node->left, key) : search(node->right, key);
+    }
+
+public:
+    BinarySearchTree() : root(nullptr) {}
+
+    void insert(K key, V value) {
+        insert(root, key, value);
+    }
+
+    V search(K key) {
+        return search(root, key);
+    }
+};
+
+#endif
--- C++ Projects/YourVeryOwnDict/Dict.hpp
+++ C++ Projects/YourVeryOwnDict/Dict.hpp
@@ -1,1 +1,37 @@
-// USE THE PHOTOS
+#ifndef DICT_HPP
+#define DICT_HPP
+
+#include <vector>
+#include "BinarySearchTree.hpp"
+
+using namespace std;
+
+template <typename K, typename V>
+class Dict
+{
+    public:
+        Dict () {}
+        void add (K key, V value);
+        V& operator[] (K key);
+    private:
+        BinarySearchTree<K,V> bt;
+};
+
+template <typename K, typename V>
+void Dict<K, V>::add(K key, V value)
+{
+    bt.insert(key, value);
+}
+
+template <typename K, typename V>
+V& Dict<K, V>::operator[](K key)
+{
+    try {
+        return bt.search(key); // Search for the key
+    } catch (runtime_error&) {
+        bt.insert(key, V());
+        return bt.search(key);
+    }
+}
+
+#endif
